8037266e6f2233fe08e080dc06198f19a1826aa0
SaiChaithanyaKummari <SaiChaithanyaKummari@users.noreply.github.com>
2026-03-12 18:00:43 +0530

    Fix Routes path casing for Linux deployment

--- Backend/server.js
+++ Backend/server.js
@@ -10,10 +10,10 @@ app.use(express.json());
 
 connectDB();
 
-app.use("/api/courses", require("./Routes/Course"));
-app.use("/api/users", require("./Routes/User"));
-app.use("/api/contact", require("./Routes/Contact"));
-app.use("/api/enrollments", require("./Routes/Enrollment"));
+app.use("/api/courses", require("./routes/Course"));
+app.use("/api/users", require("./routes/User"));
+app.use("/api/contact", require("./routes/Contact"));
+app.use("/api/enrollments", require("./routes/Enrollment"));
 
 app.listen(process.env.PORT, () => {
     console.log("Server running on port " + process.env.PORT);
